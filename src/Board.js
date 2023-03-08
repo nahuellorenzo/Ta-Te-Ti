510bc7da4e74a4c8c1deedca526b505c41a3a8f5
Nahuel Lorenzo <nahuelorenzo@gmail.com>
2023-03-08 02:24:52 -0300

    agrego funcionalidad restart game

--- src/Board.js
+++ src/Board.js
@@ -4,8 +4,13 @@ import Square from './Square';
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+  const [gameOver, setGameOver] = useState(false);
 
   function handleClick(i) {
+    if (gameOver) {
+      return;
+    }
+
     const newSquares = squares.slice();
     if (calculateWinner(newSquares) || newSquares[i]) {
       return;
@@ -13,19 +18,54 @@ function Board() {
     newSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(newSquares);
     setXIsNext(!xIsNext);
+
+    if (calculateWinner(newSquares) || newSquares.every(square => square !== null)) {
+      setGameOver(true);
+    }
   }
 
   function renderSquare(i) {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   }
 
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  function handleRestart() {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
+    setGameOver(false);
+  }
+
   const winner = calculateWinner(squares);
-  const status = winner
-    ? `Winner: ${winner}`
-    : `Next player: ${xIsNext ? 'X' : 'O'}`;
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else if (gameOver) {
+    status = 'Game over. It was a tie!';
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
 
   return (
-    <div>
+    <div className='cont'>
       <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
@@ -42,29 +82,13 @@ function Board() {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
+      <button className="restart-button" onClick={handleRestart}>
+        Restart game
+      </button>
     </div>
   );
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
 export default Board;
+
 
